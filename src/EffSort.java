9c048cee2750cbdd74e7f68ab76dc8d344edc63f
ngthgph <ngthgph112@gmail.com>
2023-04-09 11:12:57 +0700

    Created as instruction

---
+++ src/EffSort.java
@@ -0,0 +1,36 @@
+public class EffSort {
+    int[] a;
+    int n;
+
+    EffSort() {}
+    EffSort(int[] b) {
+        n = b.length;
+        a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = b[i];
+    }
+
+    void display() {
+        for(int j : a)
+            System.out.print(" " + j);
+        System.out.println();
+    }
+
+    void swap(int[] a, int i, int k) {
+        int temp = a[i];
+        a[i] = a[k];
+        a[k] = temp;
+    }
+
+    int partition(int low, int up) {
+        return 0;
+    }
+
+    void quickSort(int low, int up) {
+
+    }
+
+    void quickSort() {
+        quickSort(0, n-1);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// G:\My Drive\LEARNING\FUNiX\6_CSD201x_02-A_VN-Algorithm\Exercises
+
+public class Main {
+    static int[] b;
+
+    static void input(int[] b) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
